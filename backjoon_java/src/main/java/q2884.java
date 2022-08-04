4214af40973abc5f3dc7eb1ca219dcbc61a8f67c
aichyu312 <aichyu312@gmail.com>
2022-08-04 21:50:14 +0900

    study : q2884 백준

--- backjoon_java/src/main/java/q2884.java
+++ backjoon_java/src/main/java/q2884.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class q2884 {
+import java.util.Scanner;
+
+public class q2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int hour = A;
+        int minute = B - 45;
+
+        if (minute < 0) {
+            minute = B + 60 - 45;
+            hour = A - 1;
+            if (hour < 0) {
+                hour = 24 - 1;
+            }
+        }
+        System.out.println(hour + " " + minute);
+    }
 }
