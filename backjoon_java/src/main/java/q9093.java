acb2216057000724f1b9167360a68dc552094cc1
aichyu312 <aichyu312@gmail.com>
2022-08-04 21:50:42 +0900

    study : q9093 백준

--- backjoon_java/src/main/java/q9093.java
+++ backjoon_java/src/main/java/q9093.java
@@ -4,30 +4,23 @@ public class q9093 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-
-        for (int i = 0; i <= T; i++) {
+        sc.nextLine();
+        for (int i = 0; i < T; i++) {
             String str = sc.nextLine();
-            String reverse = "";
 
             String[] arr = str.split(" ");
+
             for (int j = 0; j < arr.length; j++) {
 
-            }
-
-
-
-            for (int j = str.length()-1; j >= 0; j--) {
-                reverse = reverse + str.charAt(j);
-            }
-            String[] arr = reverse.split(" ");
-            for (int j = arr.length-1; j >= 0; j--) {
-
-                if(j != 0){
-                    System.out.print(arr[j]+" ");
+                if (j == arr.length - 1) {
+                    System.out.println(new StringBuilder(arr[j]).reverse());
                 } else {
-                    System.out.print(arr[j]);
+                    System.out.print(new StringBuilder(arr[j]).reverse() + " ");
                 }
             }
         }
+
+
     }
 }
+
