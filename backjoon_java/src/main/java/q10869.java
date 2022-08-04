6fb2ba1430e81b4c091569b633f5527f080f378e
aichyu312 <aichyu312@gmail.com>
2022-08-04 21:49:40 +0900

    study : q10869 백준

--- backjoon_java/src/main/java/q10869.java
+++ backjoon_java/src/main/java/q10869.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class q10869 {
+import java.util.Scanner;
+
+public class q10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+
+    }
 }
+
